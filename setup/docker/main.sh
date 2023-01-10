dd49efe5d4b853bc17386db4a35af64348ea44ba
Jared Boone <jared.boone@sony.com>
2023-01-10 10:23:29 -0800

    EXPORTER_REDIS_URL_FILE

--- setup/docker/main.sh
+++ setup/docker/main.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# if EXPORTER_REDIS_URL isn't set and EXPORTER_REDIS_URL_FILE is set, read URL from file
+if [ -z "${EXPORTER_REDIS_URL-}" ] && [ -n "${EXPORTER_REDIS_URL_FILE-}" ] && [ -r "$EXPORTER_REDIS_URL_FILE" ]; then
+    EXPORTER_REDIS_URL=$(cat $EXPORTER_REDIS_URL_FILE)
+fi
+
 url="${EXPORTER_REDIS_URL:-redis://localhost:6379/0}"
 prefix="${EXPORTER_PREFIX:-bull}"
 metric_prefix="${EXPORTER_STAT_PREFIX:-bull_queue_}"
